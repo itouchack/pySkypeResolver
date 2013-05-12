e7c7bc7cea5444081b23acb228b55bdf90759286
Zarek Jenkinson <aki@aki.pw>
2013-05-13 03:19:34 +1200

    blah

--- setup.py
+++ setup.py
@@ -2,15 +2,11 @@ import py2exe
 from distutils.core import setup
 
 py2exe_options = dict(
-					excludes=['_ssl', 'doctest', 'pdb', 'difflib', 'inspect', 'calendar', 'pickle'],
-					bundle_files=1,
-					compressed=True,
-					dll_excludes=['msvcr71.dll']
-
-
-
-
-					 )
+    excludes=['_ssl', 'doctest', 'pdb', 'difflib', 'inspect', 'calendar', 'pickle'],
+	bundle_files=1,
+	compressed=True,
+	dll_excludes=['msvcr71.dll'],
+)
 
 setup(
     options = {'py2exe': py2exe_options},
