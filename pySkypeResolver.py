9a4a117f0a848cc508045ca6cef9908d5381fba7
bn0x <0_bn0x@hushmail.com>
2013-05-12 06:17:24 -0700

    Working now, need to specify .log for right now

--- pySkypeResolver.py
+++ pySkypeResolver.py
@@ -1,17 +1,22 @@
 import Skype4Py
 import sys
+import pyResolve
+import socket
 from time import sleep
 def resolve(skypeName):
 	try:
 		skype = Skype4Py.Skype()
 		skype.Attach()
-		skype.Client.OpenUserInfoDialog(sys.argv[1])
+		skype.Client.Focus()
+		skype.Client.OpenUserInfoDialog(skypeName)
 		skype.Client.Minimize()
 		skype.Client.Focus()
 		sleep(1)
-
+		resolving = pyResolve.search('debug-20130512-0558.log', skypeName)
+		publicIp = resolving[0]['public']
+		localIp = resolving[0]['local']
 		return { 'success': True, 'publicIp': publicIp, 'localIp': localIp }
-	except:
+	except socket.error:
 		print('Usage: \n	pySkypeResolver.py <SKYPEUSERNAME>')
 		print('Example: \n	pySkypeResolver.py echo123')
 		print('\nThe client flickering is for a 24/7 resolve.\n')
